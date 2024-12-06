839135154cd2fdc2eeda437fe78a6752680feff6
Alexis <gog20@icloud.com>
2024-12-06 09:59:01 +0200

    nothing

---
+++ HandShake/src/routes/users/dist/userRoute.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var express_1 = require("express");
+var setUser_1 = require("../../controllers/users/setUser");
+var router = express_1["default"].Router();
+router.post("/register", setUser_1.register).post("/login", setUser_1.login);
+exports["default"] = router;
--- HandShake/src/routes/users/userRoute.ts
+++ HandShake/src/routes/users/userRoute.ts
@@ -5,4 +5,5 @@ const router = express.Router();
 
 router.post("/register", register).post("/login", login);
 
+
 export default router;
