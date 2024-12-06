a094f2d33a53dda6757fb7f97ad9e3e6d2b0f8fe
Alexis <gog20@icloud.com>
2024-12-06 12:20:19 +0200

    model improved

--- HandShake/src/models/movies/movieModel.ts
+++ HandShake/src/models/movies/movieModel.ts
@@ -1,35 +1,5 @@
 import mongoose, { Schema, Document, model } from "mongoose";
 
-<<<<<<< Updated upstream
-export const movieSchema = new Schema({
-  id: { type: String, required: true },
-  name: {
-    type: String,
-    required: true,
-  },
-  director: {
-    type: String,
-    required: true,
-  },
-  year: {
-    type: Number,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  imageUrl: {
-    type: String,
-    required: true,
-  },
-  genre: {
-    type: String,
-    required: true,
-  },
-  ischosen: {  type: Boolean, required: true, default: false },
-  
-=======
 interface IMovie extends Document {
     title: string;
     year: number;
@@ -46,7 +16,7 @@ const MovieSchema: Schema = new Schema({
     director: { type: String, required: true },
     rating: { type: Number, required: true },
     imageURL: { type: String, required: true },
->>>>>>> Stashed changes
+
 });
 
 const Movie = model<IMovie>("Movie", MovieSchema);
