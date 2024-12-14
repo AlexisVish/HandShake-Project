5c502c4f79cd6c47487791b24c0629ea2ac997c0
Alexis <gog20@icloud.com>
2024-12-14 14:58:38 +0200

    db improved

--- HandShake/src/dist/server.js
+++ HandShake/src/dist/server.js
@@ -6,21 +6,23 @@ var mongoose_1 = require("mongoose");
 var app = express_1["default"]();
 var port = 3000;
 app.use(express_1["default"].json());
-app.use(express_1["default"].static('public'));
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+app.use(express_1["default"].static("public"));
+app.get("/", function (req, res) {
+    res.send("Hello World!");
 });
-//using mongoDB 
+//using mongoDB
 var dbUrl = "mongodb+srv://alexis:Vivalexxxa@cluster0.yzu9p.mongodb.net/";
 var database = "HandShake";
-mongoose_1["default"].connect(dbUrl + "/" + database).then(function () {
+mongoose_1["default"]
+    .connect(dbUrl + "/" + database)
+    .then(function () {
     console.info("DB connected");
 })["catch"](function (err) {
     console.error(err);
 });
 var sqlite3_1 = require("sqlite3");
 //using SQL inner DB
-exports.db = new sqlite3_1["default"].Database(':memory:', function (err) {
+exports.db = new sqlite3_1["default"].Database(":memory:", function (err) {
     if (err) {
         console.error("Error connecting to the database:", err.message);
     }
@@ -28,11 +30,14 @@ exports.db = new sqlite3_1["default"].Database(':memory:', function (err) {
         console.log("Connected to the SQLite database.");
     }
 });
+exports.db.run("CREATE TABLE IF NOT EXISTS movies (\n          id INTEGER PRIMARY KEY AUTOINCREMENT,\n          title TEXT NOT NULL,\n          genre TEXT NOT NULL,\n          year INTEGER,\n          image_path TEXT\n        )");
+exports.db.run("CREATE TABLE IF NOT EXISTS meetings (\n        id INTEGER PRIMARY KEY AUTOINCREMENT,\n        user1_id INTEGER NOT NULL,\n        user2_id INTEGER NOT NULL,\n \n      )");
+exports.db.run("CREATE TABLE IF NOT EXISTS handShake (\n        id INTEGER PRIMARY KEY AUTOINCREMENT,\n        meeting_id INTEGER NOT NULL,\n        user1_id INTEGER NOT NULL,\n        user1_id INTEGER NOT NULL,\n        movie_id INTEGER NOT NULL,\n      )");
 module.exports = exports.db;
 var userRoute_1 = require("./routes/users/userRoute");
 app.use("/api/users", userRoute_1["default"]);
 var movieRoute_1 = require("./routes/movies/movieRoute");
-app.use('/api/movies', movieRoute_1["default"]);
+app.use("/api/movies", movieRoute_1["default"]);
 app.listen(port, function () {
     console.log("App listening on port " + port);
 });
--- HandShake/src/server.ts
+++ HandShake/src/server.ts
@@ -1,44 +1,68 @@
-import express from 'express'
-import mongoose from 'mongoose';
-const app = express()
+import express from "express";
+import mongoose from "mongoose";
+const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-//using mongoDB 
+//using mongoDB
 const dbUrl = "mongodb+srv://alexis:Vivalexxxa@cluster0.yzu9p.mongodb.net/";
 const database = "HandShake";
 
-mongoose.connect(`${dbUrl}/${database}`).then(()=>{
-    console.info("DB connected")
-}).catch((err)=>{
-    console.error(err)
-});
+mongoose
+  .connect(`${dbUrl}/${database}`)
+  .then(() => {
+    console.info("DB connected");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 import sqlite3 from "sqlite3";
 
 //using SQL inner DB
-export const db = new sqlite3.Database(':memory:', (err) => {
+export const db = new sqlite3.Database(":memory:", (err) => {
   if (err) {
     console.error("Error connecting to the database:", err.message);
   } else {
     console.log("Connected to the SQLite database.");
   }
 });
+
+    db.run(`CREATE TABLE IF NOT EXISTS movies (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          title TEXT NOT NULL,
+          genre TEXT NOT NULL,
+          year INTEGER,
+          image_path TEXT
+        )`);
+
+    db.run(`CREATE TABLE IF NOT EXISTS meetings (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user1_id INTEGER NOT NULL,
+        user2_id INTEGER NOT NULL,
+      )`);
+    db.run(`CREATE TABLE IF NOT EXISTS handShake (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        meeting_id INTEGER NOT NULL,
+        user1_id INTEGER NOT NULL,
+        user1_id INTEGER NOT NULL,
+        movie_id INTEGER NOT NULL,
+      )`);
+  
 module.exports = db;
 
-
-import userRoute from './routes/users/userRoute';
+import userRoute from "./routes/users/userRoute";
 app.use("/api/users", userRoute);
 
-import movieRoute from './routes/movies/movieRoute';
-app.use('/api/movies', movieRoute);
+import movieRoute from "./routes/movies/movieRoute";
+app.use("/api/movies", movieRoute);
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`)
-  })
+  console.log(`App listening on port ${port}`);
+});
